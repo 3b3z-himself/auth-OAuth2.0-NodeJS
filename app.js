780b348c14a54240e9f45ed84cd4b8ab7bea1459
Ahmed A. El-Otify <yukkiicode@gmail.com>
2023-11-12 20:50:55 +0200

    passport, cookies, sessions, logout and much more

--- app.js
+++ app.js
@@ -4,22 +4,33 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const md5 = require('md5');
-const User = require('./models/userSchema');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
 const app = express();
 
 app.use(express.static('public'));
 
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: "My mystery secret bla bla bla",
+    resave: false,
+    saveUninitialized: false,
+
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+const User = require('./models/userSchema');
 
 mongoose.connect(process.env.DB_URI)
     .then((result) => app.listen(3000))
     .catch(err => console.log('Error connecting', err));
 
-
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -34,39 +45,51 @@ app.get('/register', (req, res) => {
     res.render('register')
 })
 
-app.post('/register', async (req, res) => {
-    try {
-        bcrypt.hash(req.body.password, saltRounds, async function (err, hashed) {
-            if (err) {
-                console.log(err);
-            }else{
-        const newUser = new User({
-            email: req.body.username,
-            password: hashed
-        });
-        await newUser.save();
-        res.render('secrets');}});
-    } catch (err) {
-        console.log(err);
+app.get('/secrets', (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render('secrets');
+    }else{
+        res.redirect('/login')
     }
+})
+
+
+app.post('/register', async (req, res) => {
+    
+    User.register(new User({ username: req.body.username }), req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect('/register');
+        } else {
+            passport.authenticate('local')(req, res, function () {
+                res.redirect('/secrets');
+            });
+        }
+    });
 });
+
+
+
 
     
 app.post('/login', function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-    User.findOne({ email: username })
-        .then(user => {
-            if (user) {
-                bcrypt.compare(password, user.password, function (err, result) {
-                    if(result === true){
-                        res.render('secrets');
-                    }
-                })
-                
-            }
-        })
-        .catch(err => {
-            console.log(err);
-        });
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user, function(err, res){
+        if(err){
+            console.log(err)
+        } else{
+            passport.authenticate('local')(req, res, function () {
+                res.redirect('/secrets');
+            });
+        }
+    })
+});
+
+
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/')
 });
--- models/userSchema.js
+++ models/userSchema.js
@@ -1,22 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 
 const UserSchema = new Schema({
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+    username: String,
+    password: String
 
 }, { timestamps: true});
 
 
 
+UserSchema.plugin(passportLocalMongoose);
 const User = mongoose.model('users', UserSchema);
-
-
+// passport.use(User.createStrategy())
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 module.exports = User;
